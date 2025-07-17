cceb33fc80ab97dec230d40b4c2e3dc7455d299a
SammyG-WSM <samgwsm1998@gmail.com>
2025-07-17 10:09:03 +0100

    Final push for course.

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    path_to_file = sys.argv[1]
 
 # Function to get the text in the document.
 def get_book_text(path_to_file):
@@ -11,13 +18,27 @@ from stats import get_word_count
 
 from stats import get_char_count
 
+from stats import sort_char_count
+
 # Main function
 def main():
-    path_to_file = "books/frankenstein.txt"
+    # Defining variables
     contents = get_book_text(path_to_file)
     count = get_word_count(contents)
-    c_count = get_char_count(contents)
+    char_count = get_char_count(contents)
+    sorted_char_counts = sort_char_count(char_count)
     
-    print(c_count)
+    # Printing the main body
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}")
+    print("----------- Word Count ----------")
+    print(f"Found {count} total words")
+    print("--------- Character Count -------")
+    # Printing the sorted list of characters
+    for item in sorted_char_counts:
+        if item["char"].isalpha():
+            print(f'{item["char"]}: {item["num"]}')
     
+    print("============= END ===============")
+            
 main()
--- stats.py
+++ stats.py
@@ -15,3 +15,15 @@ def get_char_count(contents):
             char_count[char] = 1
             
     return char_count
+
+# Function to sort the individual characters by frequency.
+
+def sort_on(item):
+    return item["num"]
+
+def sort_char_count(char_count):
+    char_dict = []
+    for character, count in char_count.items():
+        char_dict.append({"char": character, "num":count})
+    char_dict.sort(reverse=True, key=sort_on)
+    return char_dict
